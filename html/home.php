258e15c2e0bd5fa76e25c1b0d9a4be58053b0fb4
eugeMolinero <euge_molinero@yahoo.com.ar>
2016-07-07 21:24:44 -0300

    Responsive

--- html/home.php
+++ html/home.php
@@ -12,6 +12,7 @@
         <div class="container">
             <h2>Comprando cualquier producto <span><img src="<?php echo PATH_IMAGES; ?>colgate.png" alt="Colgate"></span> <span><img src="<?php echo PATH_IMAGES; ?>palmolive.png" alt="Palmolive"></span> <span><img src="<?php echo PATH_IMAGES; ?>protex.png" alt="Protex"></span> podés ganar un viaje a El Calafate.</h2>
             <img class="calafate" src="<?php echo PATH_IMAGES; ?>calafate.png" alt="Calafate">
+            <div class="clear"></div>
         </div>
     </div>
 	
