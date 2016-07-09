5d73ffd34bf295c372b68cf8d45dea2df6be4be8
Maximiliano Angeletti <max.angletti@gmail.com>
2016-07-09 11:49:36 -0300

    agregado del analytics

--- html/master.php
+++ html/master.php
@@ -61,6 +61,16 @@ require_once (__DIR__) . '/../php/config.php';
         <script type="text/javascript" src="<?php echo PATH_JS ?>jquery.fancybox.pack.js"></script>
     </head>
     <body>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		 
+		  ga('create', 'UA-73999373-3', 'auto');
+		  ga('send', 'pageview');
+		 
+		</script>
         <header>
         <div class="container">
             <nav>
