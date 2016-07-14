fa3fd0c519cafa6b8b344a41070bc7d71508c82a
eugeMolinero <euge_molinero@yahoo.com.ar>
2016-07-14 19:39:21 -0300

    Estilos

--- html/confirma.php
+++ html/confirma.php
@@ -20,23 +20,12 @@ header('Location: '.PATH_HOME);
         <link href="<?php echo PATH_CSS; ?>styles.css" rel="stylesheet" media="screen">
         <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,400italic,300,300italic,500italic,700,700italic,900' rel='stylesheet' type='text/css'>
-
         
         <script src="<?php echo PATH_JS ?>jquery-1.8.0.min.js"></script>
         <script type="text/javascript">
             var PATH_CONTROLLER = '<?php echo PATH_CONTROLLER ?>';
             var PATH_HOME = '<?php echo PATH_HOME ?>';
         </script> 
-        <script src="<?php echo PATH_JS ?>jquery.flexslider.js"></script>
-        <script>
-            $(window).load(function () {
-                $('.flexslider').flexslider({
-                    animation: "fade",
-                    slideshowSpeed: 3000,
-                    animationDuration: 500,
-                });
-            });
-        </script>
 
         <!-- anclas -->
         <script type="text/javascript">
@@ -72,21 +61,6 @@ header('Location: '.PATH_HOME);
                 });
             })
         </script>
-
-        <!-- Include Owl Carousel -->
-        <script src="<?php echo PATH_JS ?>owl.carousel.min.js"></script>
-        <script>
-            $(document).ready(function () {
-                $("#owl-demo").owlCarousel({
-                    autoPlay: false, //Set AutoPlay to 3 seconds
-                    nav:true,
-                    items: 3,
-                    itemsDesktopSmall: [979, 3],
-                    itemsTablet: [768, 2],
-                    itemsMobile: [479, 1]
-                });
-            });
-        </script>
         
     </head>
     <body>
@@ -111,15 +85,15 @@ header('Location: '.PATH_HOME);
             ga('send', 'pageview');
         </script>
         <div style="max-width:p00px; height:400px; margin:50px auto; padding-top:7%;text-align:center; background:#fff; border-radius:40px; ">
-            <h2 style="font-size: 42px; color:red; font-weight:normal">Gracias por participar!</h2>
+            <h2 style="font-size: 42px; color:#3d77b7; font-weight:normal">Gracias por participar!</h2>
             <br><br>
             <a href="<?php echo PATH_HOME ?>" class="btn" style="margin-bottom:2em">Volver al sitio</a>
 
             <!-- FOOTER -->
-            <footer>
-                <p class="copyright"> © 2016 <a href="http://www.colgatepalmolive.com.ar" target="_blank">Colgate-Palmolive Company</a>. Todos los derechos reservados. Usted está viendo la página de Argentina. <br>Vea nuestras <a href="http://www.colgate.com.ar/app/Colgate/AR/Corp/LegalPrivacy.cvsp" target="_blank">Políticas de Privacidad</a>. </p> 
-                <p class="brand"> <a href="http://www.colgatepalmolive.com.ar" target="_blank"></a> </p>
-            </footer>
+        <footer>
+            <p class="copyright"> © 2016 <a href="http://www.colgatepalmolive.com.ar" target="_blank">Colgate-Palmolive Company</a>. Todos los derechos reservados. Usted está viendo la página de Argentina. <br>Vea nuestras <a href="http://www.colgate.com.ar/app/Colgate/AR/Corp/LegalPrivacy.cvsp" target="_blank">Políticas de Privacidad</a>. </p> 
+            <p class="brand"> <a href="http://www.colgatepalmolive.com.ar" target="_blank"></a> </p>
+        </footer>
 
         </div>
 
