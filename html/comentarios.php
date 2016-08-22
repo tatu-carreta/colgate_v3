43c4532226f70e2bb4fb72e3787008ebfeb1c9ad
eugeMolinero <euge_molinero@yahoo.com.ar>
2016-08-22 17:02:18 -0300

    Adaptacion comentarios

--- html/comentarios.php
+++ html/comentarios.php
@@ -6,20 +6,43 @@
         <div class="containerComentarios">
         	<ul class="opiniones">
         		<li>
-        			<p>“...En casa regamos las plantas del jardín con una regadera, así no deperdiciamos agua”.</p>
+        			<p>“...En casa regamos las plantas del jardín con una regadera, así no deperdiciamos agua.”</p>
     				<p class="participante">María, Quilmes</p>
         		</li>
         		<li>
-        			<p>“...Utilizo un recipiente con agua caliente y detergente para lavar los platos, y abro la canilla para enjuagarlos sólamente”.</p>
+        			<p>“...Utilizo un recipiente con agua caliente y detergente para lavar los platos, y abro la canilla para enjuagarlos solamente.”</p>
     				<p class="participante">Karina, San Isidro</p>
         		</li>
         		<li>
-        			<p>“...Baño a mi perro en un fuentón que lleno una sóla vez de agua, y la uso para mojarlo y enjuagarlo después del shampú ”.</p>
+        			<p>“...Baño a mi perro en un fuentón que lleno una sóla vez de agua, y la uso para mojarlo y enjuagarlo después del shampú.”</p>
     				<p class="participante">José, Bahía Blanca</p>
         		</li>
                 <li>
-                    <p>...En la ducha diaria, abro la canilla recién cuando estoy debajo del duchador. Además soy consciente en el cuidado del agua no solamente en casa, sino también cuando voy a cualquier lugar.</p>
+                    <p>“...En la ducha diaria, abro la canilla recién cuando estoy debajo del duchador. Además soy consciente en el cuidado del agua no solamente en casa, sino también cuando voy a cualquier lugar.”</p>
                     <p class="participante">Elda, Olivos</p>
+                </li>
+                <li><p>"Al lavar el auto, uso hidrolavadora para optimizar el uso del agua.”</p>
+                    <p class="participante">Gonzalo, Capital Federal</p>
+                </li>
+                <li>
+                    <p>"Cada vez que me cepillo los dientes cierro la canilla mientras me los estoy cepillando.”</p>
+                    <p class="participante">Giuliana, Avellaneda</p>
+                </li>
+                <li>
+                    <p>"A la hora de lavar los platos primero enjabono todo y luego enjuago.”</p>
+                    <p class="participante">Dolores, Capital Federal</p>
+                </li>
+                <li>
+                    <p>"Cierro las canillas durante el cepillado de dientes y cuando utilizo el lavarropas trato de que llegue a su capacidad máxima.”</p>
+                    <p class="participante">Darío, Capital Federal</p>
+                </li>
+                <li>
+                    <p>"Junto agua de lluvia para regar las plantas."</p>
+                    <p class="participante">Daniel, Lanús</p>
+                </li>
+                <li>
+                    <p>"Mi compromiso cuidando el agua es cerrar las canillas que quedan abiertas en lugares públicos, bañarme en menos de 8 minutos, abrir el agua para lavarme las manos y cerrarla mientras no la use. El agua dulce algún día se podrá extinguir, y es lo más importante que tenemos.”</p>
+                    <p class="participante">Federico, Corrientes</p>
                 </li>
         	</ul>
         </div>
